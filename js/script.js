d8078153fbf209521c64c8065986065221ef7583
Emily Mattison <egmatti@gmail.com>
2020-10-05 21:05:12 -0400

    Moved over recent portfolio files from secondary repo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-// GETTING STARTED EXAMPLE
-// $(document).ready(function(){
-//   // jQuery code
-// });
 
 
 // MEDIA QUERIES
@@ -14,9 +10,71 @@ const mediaQueryMedium = window.matchMedia( "(max-width: 1025px)" );
 
 $(".featured-project-slider").slick({
  arrows: true,
+ cssEase: 'linear',
+ fade: true,
  infinite: true,
  slidesToScroll: 1,
  slidesToShow: 1,
+ vertical: false
+});
+
+
+// MEDIUM WIDTH PROJECT IMAGE SLIDER
+
+$(".project-slider--medium-width").slick({
+ arrows: true,
+ cssEase: 'linear',
+ fade: true,
+ infinite: true,
+ slidesToScroll: 1,
+ slidesToShow: 1,
+ vertical: false
+});
+
+
+// FULL WIDTH PROJECT IMAGE SLIDER
+
+$('#creation-flows-page .project-slider--full-width').slick({
+ arrows: true,
+ asNavFor: '#creation-flows-page .project-slider--full-width-nav',
+ cssEase: 'linear',
+ fade: true,
+ infinite: true,
+ slidesToScroll: 1,
+ slidesToShow: 1
+});
+
+$('#creation-flows-page .project-slider--full-width-nav').slick({
+ arrows: false,
+ asNavFor: '#creation-flows-page .project-slider--full-width',
+ centerMode: false,
+ dots: false,
+ focusOnSelect: true,
+ infinite: false,
+ slidesToScroll: 2,
+ slidesToShow: 2,
+ vertical: false
+});
+
+$('#tasks-and-playbooks-page .project-slider--full-width').slick({
+ arrows: true,
+ asNavFor: '#tasks-and-playbooks-page .project-slider--full-width-nav',
+ cssEase: 'linear',
+ fade: true,
+ infinite: true,
+ slidesToScroll: 1,
+ slidesToShow: 1
+});
+
+$('#tasks-and-playbooks-page .project-slider--full-width-nav').slick({
+ arrows: false,
+ asNavFor: '#tasks-and-playbooks-page .project-slider--full-width',
+ centerMode: false,
+ dots: false,
+ focusOnSelect: true,
+ infinite: false,
+ slidesToScroll: 3,
+ slidesToShow: 3,
  vertical: false
 });
 
